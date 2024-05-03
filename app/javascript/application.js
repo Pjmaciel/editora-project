3cbb411ed2de1c754f1b3a09f1be0f909166de58
Pjmaciel <pjmaciel_4@hotmail.com>
2024-05-02 21:32:56 -0300

    Reiniciar o projeto Rails na branch develop

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+//= require jquery3
+//= require bootstrap
 import "./controllers"
 import * as bootstrap from "bootstrap"

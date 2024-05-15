b26e99fb272b8031af6ce4c48d2a63c55cfe5c83
Pjmaciel <pjmaciel_4@hotmail.com>
2024-05-15 01:28:55 -0300

    feat: Implement Endpoint Routing by Authors.

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :authors
+    end
+  end
 end
